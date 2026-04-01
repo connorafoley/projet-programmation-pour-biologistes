4c3cdec6cb058789e216f04a4d55b716a21dc1f2
connorafoley <connorafoley@gmail.com>
2026-04-01 10:39:16 -0500

    modified but unfinished

--- python_scripts/fibbonacci.py
+++ python_scripts/fibbonacci.py
@@ -1,24 +1,33 @@
 import argparse
 
-
-parser =argparse.ArgumentParser(description="Haec scripta computat numerum in locis lectis in seriem fibonacciem")
-
-parser.add_argument("pos", help = 'Positio in serie fibbonacie.', type=int)
-
-parser.add_argument("-v","--verbose", help = "Proventum verbosum (longus) imprimat.",action = 'store_true')
-
-args = parser.parse_args()
-
-#pos input('enter position in the fibonacci sequence: ')
-
-a,b = 0,1
-
-for i in range(int(args.pos)):
-    a,b = b,a+b
+def get_args():
+    parser = argparse.ArgumentParser(description="Haec scripta computat numerum in locis lectis in seriem fibonacciem")
     
+    parser.add_argument("pos", help = 'Positio in serie fibbonacie.', type=int)
+    
+    parser.add_argument("-v","--verbose", help = "Proventum verbosum (longus) imprimat.",action = 'store_true')
+    
+    return parser.parse_args()
+
+def fib():
+    global fibo
+    a,b = 0,1
+    for i in range(int(args.pos)):
+        a,b = b,a+b
     fibo = a 
-if args.verbose:
-    print(f"Numerum fibonaccem in positione {args.pos} est {fibo}.")
-else: 
-    print(f"{fibo}")
     
+def fibout():
+    if args.verbose:
+        print(f"Numerum fibonaccem in positione {args.pos} est {fibo}.")
+    else: 
+        print(f"{fibo}")
+
+def main():
+    global args
+    args = get_args()
+    fib()
+    fibout()
+# Aedificatur mundum 
+
+if __name__ == "__main__":
+    main()
